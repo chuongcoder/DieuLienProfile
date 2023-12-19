1404a7f2ef731c2bdcd3c902d8c988108fd6542c
chuongdev <chuongminh3225@gmail.com>
2023-12-19 10:25:39 +0700

    fixprofile

---
+++ profile.js
@@ -0,0 +1,7 @@
+var Typed = new Typed (".multiple-text",{
+    strings: ["một Tiktoker","một sinh viên","a Cute girl :D"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
