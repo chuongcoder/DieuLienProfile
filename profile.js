261c1828e74d4896ecc20938d09f9f8172fdd4ed
chuongdev <chuongminh3225@gmail.com>
2023-12-19 11:28:20 +0700

    fix

--- profile.js
+++ profile.js
@@ -5,3 +5,15 @@ var Typed = new Typed (".multiple-text",{
     backDelay: 1000,
     loop:true
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Truy cập thẻ audio
+    var audio = document.getElementById("noelAudio");
+
+    // Bắt đầu chơi nhạc khi trang web tải xong
+    audio.play();
+});
+document.addEventListener("click", function() {
+    var audio = document.getElementById("noelAudio");
+    audio.play();
+  });
