2a21e36354bde2a79d105001e7633de214bab45c
chuongdev <chuongminh3225@gmail.com>
2024-05-12 00:40:43 +0700

    edit

--- profile.js
+++ profile.js
@@ -1,5 +1,5 @@
 var Typed = new Typed (".multiple-text",{
-    strings: ["một Tiktoker","một sinh viên","a Cute girl :D"],
+    strings: ["một sinh viên","a Cute girl :D"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
